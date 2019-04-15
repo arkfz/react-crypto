8afa3b088f88e628f9a1f074be8eb4b545af7a39
Arkadiusz Fuz <arkadiusz.fuz@gmail.com>
2019-04-15 19:54:52 +0200

    Updates

---
+++ precache-manifest.cc9215928356d0824034321d783830dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed43bc121ef1eadbe8f8fc5542a0b6a8",
+    "url": "/react-crypto/static/media/logo.ed43bc12.svg"
+  },
+  {
+    "revision": "b8d28564aa1996602d02",
+    "url": "/react-crypto/static/js/runtime~main.b0a383fd.js"
+  },
+  {
+    "revision": "24879bac810e9a17908d",
+    "url": "/react-crypto/static/js/main.d55a0b7d.chunk.js"
+  },
+  {
+    "revision": "dd0d7e79b02c3dd57435",
+    "url": "/react-crypto/static/js/2.addefd3f.chunk.js"
+  },
+  {
+    "revision": "24879bac810e9a17908d",
+    "url": "/react-crypto/static/css/main.01802fd5.chunk.css"
+  },
+  {
+    "revision": "837456ebb4750f798c997672b51396d5",
+    "url": "/react-crypto/index.html"
+  }
+];
